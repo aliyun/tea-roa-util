2066a3bbf8cd02e660bf7665868958e3817a1d55
824750130 <e824750130@qq.com>
2020-05-09 10:32:40 +0800

    fix static method call rules

--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -1,12 +1,12 @@
 import unittest
 from Tea.request import TeaRequest
-from alibabacloud_roa_util import client
+from alibabacloud_roa_util.client import Client
 
 
 class TestClient(unittest.TestCase):
     def test_get_str_to_sign(self):
         request = TeaRequest()
-        str_to_sign = client.get_str_to_sign(request)
+        str_to_sign = Client.get_str_to_sign(request)
         self.assertEqual('GET\n\n\n\n\n', str_to_sign)
 
         request = TeaRequest()
@@ -14,14 +14,14 @@ class TestClient(unittest.TestCase):
         request.query = {
             'test': 'tests'
         }
-        str_to_sign = client.get_str_to_sign(request)
+        str_to_sign = Client.get_str_to_sign(request)
         self.assertEqual('POST\n\n\n\n\n?test=tests', str_to_sign)
 
         request = TeaRequest()
         request.headers = {
             'content-md5': 'md5',
         }
-        str_to_sign = client.get_str_to_sign(request)
+        str_to_sign = Client.get_str_to_sign(request)
         self.assertEqual('GET\n\nmd5\n\n\n', str_to_sign)
 
         request = TeaRequest()
@@ -37,14 +37,14 @@ class TestClient(unittest.TestCase):
             'content-type': 'application/json',
             'date': 'date'
         }
-        str_to_sign = client.get_str_to_sign(request)
+        str_to_sign = Client.get_str_to_sign(request)
         s = 'GET\napplication/json\nmd5\napplication/json\ndate\nx-acs-meta:user\n?ccp=ok&test=tests'
         self.assertEqual(s, str_to_sign)
 
     def test_get_signature(self):
         request = TeaRequest()
-        str_to_sign = client.get_str_to_sign(request)
-        signature = client.get_signature(str_to_sign, 'secret')
+        str_to_sign = Client.get_str_to_sign(request)
+        signature = Client.get_signature(str_to_sign, 'secret')
         self.assertEqual('GET\n\n\n\n\n', str_to_sign)
         self.assertEqual('XGXDWA78AEvx/wmfxKoVCq/afWw=', signature)
 
