d1fd6ed47cfdf2572b7510d550c36a84c868c48b
824750130 <e824750130@qq.com>
2020-06-23 13:54:56 +0800

    Fix python unit test

--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -4,9 +4,9 @@ from alibabacloud_roa_util.client import Client
 
 
 class TestClient(unittest.TestCase):
-    def test_get_str_to_sign(self):
+    def test_get_string_to_sign(self):
         request = TeaRequest()
-        str_to_sign = Client.get_str_to_sign(request)
+        str_to_sign = Client.get_string_to_sign(request)
         self.assertEqual('GET\n\n\n\n\n', str_to_sign)
 
         request = TeaRequest()
@@ -14,14 +14,14 @@ class TestClient(unittest.TestCase):
         request.query = {
             'test': 'tests'
         }
-        str_to_sign = Client.get_str_to_sign(request)
+        str_to_sign = Client.get_string_to_sign(request)
         self.assertEqual('POST\n\n\n\n\n?test=tests', str_to_sign)
 
         request = TeaRequest()
         request.headers = {
             'content-md5': 'md5',
         }
-        str_to_sign = Client.get_str_to_sign(request)
+        str_to_sign = Client.get_string_to_sign(request)
         self.assertEqual('GET\n\nmd5\n\n\n', str_to_sign)
 
         request = TeaRequest()
@@ -37,13 +37,13 @@ class TestClient(unittest.TestCase):
             'content-type': 'application/json',
             'date': 'date'
         }
-        str_to_sign = Client.get_str_to_sign(request)
+        str_to_sign = Client.get_string_to_sign(request)
         s = 'GET\napplication/json\nmd5\napplication/json\ndate\nx-acs-meta:user\n?ccp=ok&test=tests'
         self.assertEqual(s, str_to_sign)
 
     def test_get_signature(self):
         request = TeaRequest()
-        str_to_sign = Client.get_str_to_sign(request)
+        str_to_sign = Client.get_string_to_sign(request)
         signature = Client.get_signature(str_to_sign, 'secret')
         self.assertEqual('GET\n\n\n\n\n', str_to_sign)
         self.assertEqual('XGXDWA78AEvx/wmfxKoVCq/afWw=', signature)
